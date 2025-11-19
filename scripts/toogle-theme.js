db28871218e2cde74cc52d301d2b74336157f948
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-11-19 19:54:06 -0300

    feat: guardar tema en local storage

--- scripts/toogle-theme.js
+++ scripts/toogle-theme.js
@@ -1,15 +1,23 @@
 const toogleThemeBtn = document.querySelector("[toogleTheme]");
 const theme = document.querySelector("[data-theme]");
 
+theme.setAttribute("data-theme", localStorage.getItem("data-theme"));
+
 toogleThemeBtn.addEventListener("click", () => {
   const currentTheme = theme.getAttribute("data-theme");
   const currentIcon = toogleThemeBtn.children[0];
 
   if (currentTheme === "dark") {
-    theme.setAttribute("data-theme", "light");
+    saveToLocalStorage("light");
     currentIcon.setAttribute("class", "fa-solid fa-sun");
   } else {
-    theme.setAttribute("data-theme", "dark");
+    saveToLocalStorage("dark");
     currentIcon.setAttribute("class", "fa-solid fa-moon");
   }
 });
+
+// BONUS, guardar tema seleccionado en local storage
+function saveToLocalStorage(themeName) {
+  localStorage.setItem("data-theme", themeName);
+  theme.setAttribute("data-theme", themeName);
+}
